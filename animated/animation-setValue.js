16db47a21206977f7d51e75e5401b6564ca3a881
Howard Yeh <howard@metacircus.com>
2016-05-15 17:44:48 -0700

    use setValue to drive animation

---
+++ animated/animation-setValue.js
@@ -0,0 +1,115 @@
+import React from "react";
+
+import {
+  AppRegistry,
+  StyleSheet,
+
+  Animated,
+
+  TouchableOpacity,
+
+  View,
+  Text,
+} from "react-native";
+
+import Easing from "Easing";
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.animated = {
+      left: new Animated.Value(0)
+    }
+  }
+
+  animateLeft = () => {
+    const currentLeft = this.animated.left._value;
+
+    if (currentLeft >= 300) {
+      return;
+    }
+
+    const dx = 16;
+    this.animated.left.setValue(currentLeft + dx);
+    requestAnimationFrame(this.animateLeft);
+  }
+
+  reset = () => {
+    this.animated.left.setValue(0);
+  }
+
+  busywork() {
+    let n = 100000000;
+    while(n > 0) {
+      n--;
+    }
+  }
+
+  renderToolbar() {
+    return (
+      <View style={jss.toolbar}>
+        <TouchableOpacity style={jss.animateTrigger}
+          onPress={this.animateLeft}
+          >
+          <Text>Animate</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={jss.animateTrigger}
+          onPress={this.reset}
+          >
+          <Text>Reset</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  render() {
+    const { left } = this.animated;
+    console.log("render");
+
+    this.busywork();
+
+    return (
+      <View style={jss.container}>
+        <Animated.View style={[jss.box, {left}]}/>
+
+        {this.renderToolbar()}
+      </View>
+    );
+  }
+}
+
+const jss = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: 'stretch',
+
+  },
+
+  box: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: 100,
+    height: 100,
+    backgroundColor: "red"
+  },
+
+  toolbar: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+
+    flexDirection: 'row',
+  },
+
+  animateTrigger: {
+    padding: 10,
+    marginLeft: 10,
+
+    backgroundColor: 'lightgray',
+  },
+});
+
+AppRegistry.registerComponent('SampleApp', () => App);
